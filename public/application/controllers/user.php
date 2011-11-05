f613844beede70975f31b7f958825c1ef5e0533e
Psyrax <psyrax@opiumgarden.org>
2011-11-05 17:40:57 -0600

    Getting user data attemp 1

--- public/application/controllers/user.php
+++ public/application/controllers/user.php
@@ -23,11 +23,31 @@ class User extends CI_Controller {
 		$this->template->load('template', 'user/index_view', $data);
 	}
 	public function back(){
+		//Getting User OAuth
 		$o_code=$_GET['code'];
+		$url = "https://github.com/login/oauth/access_token";
+		$postData = array(
+			"client_id" => "bd01925d961b7962f327", 
+			"client_secret" => "ec02e79e203ada11e82318c1d69e3bee2e8d12fb"
+		);
+		/*Convierte el array en el formato adecuado para cURL*/
+		$elements = array();
+		foreach ($postData as $name=>$value) {
+			$elements[] = "{$name}=".urlencode($value);
+		}
+		$handler = curl_init();
+		curl_setopt($handler, CURLOPT_URL, $url);
+		curl_setopt($handler, CURLOPT_POST,true);
+		curl_setopt($handler, CURLOPT_POSTFIELDS, $elements);
+		$response = curl_exec ($handler);
+		curl_close($handler);
+
+
+		/*
 		$handler = curl_init("https://api.github.com/user?access_token".$o_code);
 		$response = curl_exec ($handler);
 		curl_close($handler);
-		echo $response;
+		echo $response;*/
 		//print_r($user_data);
 	}
 }
