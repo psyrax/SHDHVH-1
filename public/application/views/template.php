d19b32c23a061406521d50e1af16742dc9682108
Rodrigo Boyzo <boyzo.joe@gmail.com>
2011-11-05 16:52:00 -0600

    style css first

---
+++ public/application/views/template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="/blueprint/style.css" type="text/css" media="screen, projection">
+  		<link rel="stylesheet" href="/blueprint/print.css" type="text/css" media="print"> 
+  	</head>
+
+<body>
+	<div class="container showgrid">
+		<div id="header" class="span-24">Header</div>
+    	<div id="contents" class="span-24"><?= $contents ?>Contents</div>
+    	<div id="footer" class="span-24">Footer</div>
+    </div>
+</body>
+</html>
