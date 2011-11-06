16c4dbe643d07f750d913beb4c1b8c0096cbc190
Rodrigo Boyzo <boyzo.joe@gmail.com>
2011-11-05 22:28:02 -0600

    Desing

---
+++ public/application/views/home/index_view.php
@@ -0,0 +1,37 @@
+<div class="reset"></div>
+
+<div class="span-24">
+	<div class="span-10 first prepend-2 append-1">
+		<div id="box">
+			<div id="form">
+				<h2>Submit</h2>
+				<p id="box_desc">Submit a website to be revamped, remodeled and redesigned!</p>
+				<form id="submit" method="post" action="" method="post" class="validate">
+					<p class="instructions">Website</p><input type="text" name="website" placeholder="http://..." required/>
+					<p class="instructions">Needs</p> <input type="text" name="needs" placeholder="design, ux, html, css, javascript" required/>
+					<p class="instructions">Submitter</p><input type="text" name="submiter" placeholder="Name" required/>
+					<p class="instructions">Description</p><input type="text" name="description" placeholder="This website needs help with... because..." required/>
+					<input type="submit" value="Submit" name="submit" class="button" />
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="span-10 last">
+		<div id="box">
+			<h2>Adopt</h2>
+			<div class="details">
+				<div class="information">
+					<p><a href="http://" >Website</a></p>
+					<p>Needs:</p>
+					<p>Submitter:</p>
+				</div>
+				<div class="thumb">
+					<img src="/images/thumb.png" width="75" height="75">
+				</div>
+				<button type="button">Browse</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="reset"></div>
--- public/application/views/template.php
+++ public/application/views/template.php
@@ -7,10 +7,25 @@
   	</head>
 
 <body>
-	<div class="container showgrid">
-		<div id="header" class="span-24">Header</div>
-    	<div id="contents" class="span-24"><?= $contents ?>Contents</div>
-    	<div id="footer" class="span-24">Footer</div>
+	<div id="body" class="container">
+		<div id="header" class="span-24">
+			<div class"logo">
+				<h1><span id="gradient"></span>Do<span id="vamp">vamp</span></h1>
+				<p id="description" class="push-5">Help us make the web more functional, clean and pretty.
+					<br />Submit and Adopt websites that need to be revamped!
+					<br />
+					<span class="small quiet" style="text-align:center; margin-left:20px">-We requiere GitHub login to submit or adopt a website in need-</span>
+				</p>
+			</div>
+		</div>
+		<div class="reset"></div>
+    	<div id="contents" class="span-24"><?= $contents ?></div>
+    	<div class="reset"></div>
+    	<div id="footer" class="span-24">
+    		<p class="span-3 prepend-8 first"><a href="/about" >About</a></p>
+    		<p class="span-3"><a href="/about" >FAQ</a></p>
+    		<p class="span-3 append-7 last"><a href="/about">GitHub</a></p>
+    	</div>
     </div>
 </body>
 </html>
