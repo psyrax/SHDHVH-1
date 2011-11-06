2259cec9890719a638e500db9ece10f90d382fb9
Psyrax <psyrax@opiumgarden.org>
2011-11-05 18:53:54 -0600

    Getting user data attempt 1

--- public/application/controllers/user.php
+++ public/application/controllers/user.php
@@ -35,6 +35,7 @@ class User extends CI_Controller {
 		$response = curl_exec ($handler);
 		curl_close($handler);
 		echo $response;
+		$res_data=explode("=", $response);
 		/*
 		$handler = curl_init("https://api.github.com/user?access_token".$o_code);
 		$response = curl_exec ($handler);
